c6e33975b29c5af3ec08f6f11230c5742270c61e
Berselaidy <berselaidy@gmail.com>
2024-08-02 07:06:36 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// iniciamos el módulo express con el fin de dar inicio al servidor, evitando varias configuraciones 
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose");
+
+// Importar ruta de usuario
+const userRoutes = require("./routes/user");
+
+require("dotenv").config();
+
+// Se configura cómo va a escuchar el servidor las peticiones 
+const port = process.env.PORT || 9000;
+app.listen(port, () => console.log('Servidor ok', port));
+
+//middleware
+app.use('/api',userRoutes);
+app.use(express.json());
+
+// Se crea una ruta 
+app.get("/",(req,res)=> {
+    res.send("API en construcción")
+
+});
+
+// Conexión a la Base de datos
+mongoose.connect(process.env.BDMONGO_URI)
+.then(()=> console.log("Conexión establecida"))
+.catch((error)=> console.log(error));
---
+++ src/routes/user.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const userSchema = require("../models/user");
+
+// Crear usuarios
+router.post ("/users",(req, res)=> {
+    const user = userSchema(req.body);
+    user
+    .save()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({mensage: error}));
+});
+
+//obtener todos los usuarios
+router.get ("/users",(req,res)=> {
+    userSchema
+    .find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({mensage: error}));
+});
+
+// Mostrar un usuario en específico
+router.get ("/users/:id",(req,res)=> {
+    const {id} = req.params;
+    userSchema
+    .findById(id)
+    .then((data) => res.json(data))
+    .catch((error) => res.json({mensage: error}));
+});
+
+//Actualizar usuario
+router.put ("/users/:id",(req,res)=> {
+    const {id} = req.params;
+    const { name, email, profesion } = req.body;
+    userSchema
+    .updateOne({_id: id }, {$set: { name, email, profesion }})
+    .then((data) => res.json(data))
+    .catch((error) => res.report.json({mensage: error}));
+});
+
+//Eliminar usuario
+router.delete ("/users/:id",(req,res)=> {
+    const {id} = req.params;
+    userSchema
+    .deleteOne({_id: id })
+    .then((data) => res.json(data))
+    .catch((error) => res.report.json({mensage: error}));
+});
+
+module.exports = router 
